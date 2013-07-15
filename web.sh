820ba8112c8752a154da459583474d69b1c2bdb9
tukiyo3 <tukiyo3@gmail.com>
2013-07-16 00:13:24 +0900

    fix:correct charset

--- web.sh
+++ web.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-TMPFILE=${HOME}/tmp.txt
+TMPFILE=${HOME}/tmp.html
+
+echo '<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"></head><body><pre>' > ${TMPFILE}
+
 
 if [ "$1" = "-n" ];then
-    cat -ns > ${TMPFILE}
+    cat -ns >> ${TMPFILE}
 else
-    cat -s > ${TMPFILE}
+    cat -s >> ${TMPFILE}
 fi
 x-www-browser ${TMPFILE} 1> /dev/null
-sleep 1
-rm ${TMPFILE}
